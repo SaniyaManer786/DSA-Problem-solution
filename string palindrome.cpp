31f2e72f6e15942e360dc7e0bf95d3268963d735
saniya maner <saniyamaner13@gmail.com>
2026-01-30 19:01:07 +0530

    Update string palindrome.cpp

--- string palindrome.cpp
+++ string palindrome.cpp
@@ -18,3 +18,28 @@ int main()
  
     return 0;
 }
+
+
+
+// using bool function
+#include <iostream>
+#include<string>
+#include <algorithm> 
+using namespace std;
+bool isplaindrome(string str1){
+    string str2=str1;
+    reverse(str2.begin(),str2.end());
+    if(str2==str1){
+    return true;
+    }else{
+        return false;
+    }
+}
+int main()
+{
+ string str1;
+ cin>>str1;
+ cout<<isplaindrome(str1)<<endl;
+ return 0;
+
+}
