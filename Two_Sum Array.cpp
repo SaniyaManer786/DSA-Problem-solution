d455f046984c132f19e99144dd47e047384c9281
saniya maner <saniyamaner13@gmail.com>
2026-01-30 19:32:32 +0530

    Update Two_Sum Array.cpp

--- Two_Sum Array.cpp
+++ Two_Sum Array.cpp
@@ -1,3 +1,4 @@
+// brute force approch
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -12,4 +13,48 @@ public:
         return {};
        } 
     
+};
+
+// better approach
+class Solution {
+  public:
+    bool twoSum(vector<int>& arr, int target) {
+        map<int,int>map;
+        
+        for(int i=0;i<arr.size();i++){
+        int a=arr[i];
+        int more=target-a;
+        if(map.find(more)!=map.end()){
+       
+            return true;
+            // return (map(more),i)
+        }
+        
+        map[a]=i;
+        }
+        return false;
+        
+    }
+};
+// optimal using sorting
+
+class Solution {
+  public:
+    bool twoSum(vector<int>& arr, int target) {
+        int n=arr.size();
+        int right=n-1;
+        int left=0;
+        sort(arr.begin(),arr.end());
+        while(left<right){
+            int sum=arr[left]+arr[right];
+            if(sum==target){
+                return true;
+            }else if(sum<target){
+                left++;
+            }else{
+                right--;
+            }
+        }
+            return false;
+        }
 };
