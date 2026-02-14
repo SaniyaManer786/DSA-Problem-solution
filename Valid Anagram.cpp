b6f73b4755e295e60e634e88a9f3e9d7a6f458ec
saniya maner <saniyamaner13@gmail.com>
2026-02-14 13:38:33 +0530

    Create Valid Anagram.cpp

---
+++ Valid Anagram.cpp
@@ -0,0 +1,38 @@
+
+//brute force approach
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+
+        if(s==t){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
+
+
+//optimal approach
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+      if(s.length()!=t.length())return false;
+
+      vector<int>freq(26,0);
+
+      for(int i=0;i<s.length();i++){
+        freq[s[i]-'a']++;
+        freq[t[i]-'a']--;
+          }
+
+          for (int i=0;i<26;i++){
+            if(freq[i]!=0){
+                return false;
+            }
+          }
+          return true;
+    }
+};
