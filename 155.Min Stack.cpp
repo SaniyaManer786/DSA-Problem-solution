82d0099c18c245a135527bcd0473110844fab471
saniya maner <saniyamaner13@gmail.com>
2026-07-25 00:08:17 +0530

    Create 155.Min Stack.cpp

---
+++ 155.Min Stack.cpp
@@ -0,0 +1,50 @@
+class MinStack {
+     long long mini;
+    stack<long long>st;
+public:
+    MinStack() {
+       mini=0;
+    }
+    
+    void push(int value) {
+        if(st.empty()){
+            mini=value;
+            st.push(value);
+        }else{
+            if(value>=mini){
+                st.push(value);
+            }else{
+             st.push(2LL*value-mini);
+             mini=value;
+            }
+        }
+    }
+    
+    void pop() {
+        if(st.empty()){
+            return;
+        }
+            long long x=st.top();
+            st.pop();
+            if(x<mini)
+            mini=2*mini-x;
+        
+    }
+    
+    int top() {
+        if(st.empty()){
+            return -1;
+        }
+            long long x=st.top();
+        if(x>=mini){
+            return x;
+        }else{
+            return mini;
+        }
+        
+    }
+    
+    int getMin() {
+        return mini;
+    }
+};
